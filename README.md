f38a6fd0ec167afc4c4b48d8f6d9ff4de706fc69
Alex Thorp <alexthorp97@icloud.com>
2021-02-13 11:43:36 +0000

    second commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# Randome messages - codecademy project 
+
+This project will randomly produce a message when ran, it will provide reason for why someone was late to an event.
+
+## Technologies 
+
+node.js
+
+## Launch
+
+To launch the project;
+
+cd ../Random Messages
+
+node script.js
--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-console.log('hello')
+const person =  ["Mary","Freya","Nick", "Jeremy", "Alex", "Sephen", "James", "Luke"];
+const event = ['to Tom and lucys wedding', 'to the board meeting', 'cooking dinner', 'making cocktails']
+const reason = ['Freya was hasseling them.','the car brokendown.', 'the flight was delayed.', 'of brexit.']
+
+const random = (arr) => {
+   ranNum = Math.floor(Math.random() * (arr.length-1)); 
+    return console.log(ranNum);
+}
+
+
+
+const randomMessage = () => {
+
+return console.log(
+    person[Math.floor(Math.random() * (person.length-1))] 
++ ' was late ' + event[Math.floor(Math.random() * (event.length-1))] 
++ ' because ' + reason[Math.floor(Math.random() * (reason.length-1))]);
+}
+
+randomMessage();
+
+
+
+
